bd6ca079dffaf3a176d957605f3fb86599b48991
button-chen <727690080@qq.com>
2018-11-14 08:27:09 +0800

    简单资源池实现

---
+++ ResPool.hpp
@@ -0,0 +1,52 @@
+﻿#pragma once
+#include <queue>
+#include <mutex>
+#include <memory>
+
+template<typename T>
+class ResPool
+{
+public:
+	ResPool(const ResPool&) = delete;
+	ResPool& operator= (const ResPool&) = delete;
+
+	ResPool(ResPool&&) = default;
+	ResPool& operator= (ResPool&&) = default;
+
+	~ResPool() = default;
+
+public:
+	ResPool(std::function<T*()> factoryfunc, size_t size) {
+		m_factory = factoryfunc;
+		m_size = size;
+	}
+
+	std::unique_ptr<T> Acquire() {
+		{
+			std::lock_guard<std::mutex> lock(m_mtx);
+			if (m_resources.size()) {
+				std::unique_ptr<T> r = std::move(m_resources.front());
+				m_resources.pop();
+				return r;
+			}
+		}
+		return std::unique_ptr<T>(m_factory());
+	}
+
+	void Release(std::unique_ptr<T>& r) {
+		{
+			std::lock_guard<std::mutex> lock(m_mtx);
+			if (m_resources.size() < m_size) {
+				m_resources.push(std::move(r));
+				return;
+			}
+		}
+		r.reset();
+	}
+
+private:
+	std::queue<std::unique_ptr<T>> m_resources;
+	std::mutex m_mtx;
+	std::function<T*()> m_factory;
+	size_t m_size;
+};
---
+++ ResPool_test.cpp
@@ -0,0 +1,55 @@
+﻿#include <iostream>
+#include <vector>
+#include <thread>
+#include <chrono>
+#include "ResPool.hpp"
+
+
+struct Conn
+{
+	Conn() {
+		v = new int;
+		*v = 100;
+	}
+	~Conn() {
+		delete v;
+	}
+	int getv() {
+		return *v;
+	}
+	int* v;
+};
+
+Conn* fun() {
+	Conn* p = new Conn;
+	return p;
+}
+
+int main()
+{
+	std::shared_ptr<ResPool<Conn>> pool = std::make_shared<ResPool<Conn>>(fun, 10);
+
+	auto f = [&pool]()->void {
+		for (int i = 0; i < 100; ++i) {
+			std::unique_ptr<Conn> r = pool->Acquire();
+			std::cout << r->getv() << std::endl;
+			std::this_thread::sleep_for(std::chrono::seconds(1));
+			pool->Release(r);
+			if (r) {
+				std::cout << "error" << std::endl;
+			}
+		}
+	};
+
+	int tcnt = 3;
+	std::vector<std::thread> v;
+	for (int i = 0; i < tcnt; ++i) {
+		v.emplace_back(std::thread(f));
+	}
+
+	for (auto&& t : v) {
+		t.join();
+	}
+
+	return 0;
+}
